3d2271fe156695894f3cdf8982fd49456a607611
luisEmarin <Luis.e.marin@outlook.com>
2021-11-05 10:19:51 -0400

    first

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     
     SECRET_KEY = "You will never guess..."
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('Deploy_Database_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
 
